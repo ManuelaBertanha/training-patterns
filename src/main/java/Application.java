7864a659b635748df8b7be425716c2a1f41d0503
ManuelaBertanha <bertanhamanuela@outlook.com>
2025-01-19 19:58:45 +0000

    Testando alguns métodos

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,20 @@
+import designPatterns.creational.abstractFactory.dairyFactory.model.Milk;
+import designPatterns.creational.abstractFactory.dairyFactory.model.semiSkimmed.SemiSkimmedMilk;
+import designPatterns.creational.abstractFactory.dairyFactory.model.skimmed.SkimmedMilk;
+import designPatterns.creational.abstractFactory.dairyFactory.model.whole.WholeMilk;
+
 public class Application {
+
     public static void main(String[] args) {
-        System.out.println("Hi");
+
+        Milk skimmedMilk = new SkimmedMilk();
+        Milk semiSkimmedMilk = new SemiSkimmedMilk();
+        Milk wholeMilk = new WholeMilk();
+
+        System.out.println(skimmedMilk.showIngredients() +
+                '\n' + semiSkimmedMilk.showIngredients() +
+                '\n' + wholeMilk.showIngredients()
+        );
     }
+
 }

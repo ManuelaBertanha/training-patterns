99aab9c676582c8ebcbdfcde2e123c774d439743
ManuelaBertanha <bertanhamanuela@outlook.com>
2025-01-19 19:47:55 +0000

    Implementação do método 'generateIngredientsList(String ingredient)'

--- src/main/java/designPatterns/creational/abstractFactory/dairyFactory/model/Milk.java
+++ src/main/java/designPatterns/creational/abstractFactory/dairyFactory/model/Milk.java
@@ -1,8 +1,18 @@
 package designPatterns.creational.abstractFactory.dairyFactory.model;
 
+import designPatterns.creational.abstractFactory.dairyFactory.enums.MilkStabilizer;
+
 public abstract class Milk extends Dairy {
 
     protected float pricePerLiter;
 
-    public abstract int getPrice();
+    @Override
+    protected void generateIngredientsList(String ingredient) {
+        ingredients.add(0, ingredient);
+        ingredients.add(1, String.valueOf(MilkStabilizer.SODIUM_CITRATE));
+        ingredients.add(2, String.valueOf(MilkStabilizer.SODIUM_TRIPHOSPHATE));
+        ingredients.add(3, String.valueOf(MilkStabilizer.SODIUM_MONOPHOSPHATE));
+        ingredients.add(4, String.valueOf(MilkStabilizer.SODIUM_DIPHOSPHATE));
+    }
+
 }

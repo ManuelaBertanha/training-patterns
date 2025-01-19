4f8375c5ef7db04a79a712699280d531ac4bfba1
ManuelaBertanha <bertanhamanuela@outlook.com>
2025-01-19 19:44:10 +0000

    Inclusão de construtor, mudança em generateIngredientsList() e inclusão do método showIngredients()

--- src/main/java/designPatterns/creational/abstractFactory/dairyFactory/model/Dairy.java
+++ src/main/java/designPatterns/creational/abstractFactory/dairyFactory/model/Dairy.java
@@ -1,9 +1,14 @@
 package designPatterns.creational.abstractFactory.dairyFactory.model;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Dairy {
+
+    public Dairy() {
+        ingredients = new ArrayList<>();
+    }
 
     protected List<String> ingredients; // TRY TO CREATE A NUTRITIONAL TABLE
     protected double fatContent;
@@ -11,8 +16,10 @@ public abstract class Dairy {
     protected DateFormat expirationDate;
     protected String batchNumber;
 
-    abstract List<String> generateIngredientsList();
-    abstract void setFatContent(double percentage);
-    abstract DateFormat calculateExpirationDate();
-    abstract String generateBatchNumber();
+    protected abstract void generateIngredientsList(String ingredient);
+    public abstract String showIngredients();
+
+    //abstract void setFatContent(double percentage);
+    //abstract DateFormat calculateExpirationDate();
+    //abstract String generateBatchNumber();
 }
